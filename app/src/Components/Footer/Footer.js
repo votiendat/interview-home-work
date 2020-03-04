037344aef1d897d1f00fa8ae0923f861af651ce3
Đạt Võ <votiendat1110@gmail.com>
2020-03-04 16:30:40 +0700

    Post listing with comment section

--- app/src/Components/Footer/Footer.js
+++ app/src/Components/Footer/Footer.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Footer = (props) => {
-    return(
-        <div className="container-fluid d-flex w-100 p-3 mx-auto flex-column">
-            <footer className="mastfoot mt-auto text-center">
-                <div className="inner">
-                <p>Copyright © 2020 by <a href="https://zigvy.com/">@Zigvy</a>.</p>
-                </div>
-            </footer>
+const Footer = props => {
+  return (
+    <div className="container-fluid d-flex w-100 mt-5 bg-light p-3 mx-auto flex-column">
+      <footer className="mastfoot mt-auto text-center">
+        <div className="inner">
+          <p className="mb-0">
+            Copyright © 2020 by{" "}
+            <a href="https://zigvy.com/" className="text-warning">
+              @Zigvy
+            </a>
+            .
+          </p>
         </div>
-    )
-}
+      </footer>
+    </div>
+  );
+};
 
 export default Footer;
--- app/src/Components/Posts/Posts.js
+++ app/src/Components/Posts/Posts.js
@@ -9,17 +9,129 @@ class Posts extends Component {
   render() {
     return (
       <div className="container posts">
-        <div className="row">
-          <div className="col">SEARCH BOX</div>
+        <div className="row search-box mt-5 mb-5">
+          <div className="col">
+            <div className="form-group has-search mb-0">
+              <span className="fa fa-search form-control-feedback"></span>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search Resources ..."
+              />
+            </div>
+          </div>
         </div>
-        <div className="row">
-          <div className="col">POSTS</div>
-          <div className="col">POSTS</div>
-          <div className="col">POSTS</div>
-          <div className="col">POSTS</div>
-          <div className="col">POSTS</div>
-          <div className="col">POSTS</div>
-          <div className="col">POSTS</div>
+
+        <div className="row posts-container flex-column">
+          <div className="col post mb-4">
+            <div className="card d-flex align-items-center flex-row bg-white p-4">
+              <div className="card-img-box">
+                <div
+                  className="card-img"
+                  style={{
+                    backgroundImage: `url(${require("../../assets/images/post-feature-image-1.jpg")})`
+                  }}
+                ></div>
+              </div>
+              <div className="card-body">
+                <p className="post-category text-warning mb-0">
+                  <strong>UI/UX</strong>
+                </p>
+
+                <div className="post-tags mt-3 mb-3">
+                  <span className="post-tag badge badge-primary">Blue</span>
+                  <span class="post-tag badge badge-secondary">Light Grey</span>
+                  <span class="post-tag badge badge-success">Green</span>
+                  <span class="post-tag badge badge-danger">Red</span>
+                  <span class="post-tag badge badge-warning">Yellow</span>
+                  <span class="post-tag badge badge-info">Info</span>
+                  <span class="post-tag badge badge-light">Light</span>
+                  <span class="post-tag badge badge-dark">Dark</span>
+                </div>
+
+                <h4 className="post-title">
+                  Lorem ipsum dolor sit amet consectetur
+                </h4>
+                <p className="post-excerpt text-secondary mt-3">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Deleniti, similique explicabo molestias cupiditate assumenda
+                  Deleniti, similique explicabo molestias cupiditate assumenda.
+                  Deleniti, similique explicabo molestias cupiditate assumenda.
+                </p>
+
+                <p className="post-credential text-secondary">
+                  Author:{" "}
+                  <a href="/user" className="text-secondary">
+                    <strong>Superman</strong>
+                  </a>{" "}
+                  - Create at: <strong>Mar 04, 2019</strong>
+                </p>
+              </div>
+            </div>
+
+            <div className="post-cmt pl-4 pr-4 pb-4">
+              <a
+                className="post-cmt-btn"
+                data-toggle="collapse"
+                href="#collapseExample"
+                role="button"
+                aria-expanded="false"
+                aria-controls="collapseExample"
+              >
+                2 replies
+              </a>
+
+              <div className="replies-container collapse" id="collapseExample">
+                <div className="replies-box card card-body d-flex flex-row">
+                  <img
+                    src={require("../../assets/images/man.svg")}
+                    alt="avatar"
+                    width="45px"
+                    height="45px"
+                    className="mr-3"
+                  />
+                  <div>
+                    <div>
+                      <h6>
+                        BATMAN{" "}
+                        <span className="text-secondary">1 days ago</span>
+                      </h6>
+                    </div>
+                    <p className="mb-0">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life
+                      accusamus terry richardson ad squid. Nihil anim keffiyeh
+                      helvetica, craft beer labore wes anderson cred nesciunt
+                      sapiente ea proident.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="replies-box card card-body d-flex flex-row">
+                  <img
+                    src={require("../../assets/images/man.svg")}
+                    alt="avatar"
+                    width="45px"
+                    height="45px"
+                    className="mr-3"
+                  />
+                  <div>
+                    <div>
+                      <h6>
+                        BATMAN{" "}
+                        <span className="text-secondary">2 days ago</span>
+                      </h6>
+                    </div>
+                    <p className="mb-0">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life
+                      accusamus terry richardson ad squid. Nihil anim keffiyeh
+                      helvetica, craft beer labore wes anderson cred nesciunt
+                      sapiente ea proident.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
